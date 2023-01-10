75ee1ca20992f35b5ff2bc03968df40b7e4b7c97
eurogig <eurogig@gmail.com>
2023-01-10 16:32:45 -0500

    fixed setup

--- ac-utils/setup.sh
+++ ac-utils/setup.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 # setup.sh
 #
-# Sets up the files required to deploy the Bridgecrew Checkov admission controller in a cluster
+# Sets up and deploys in the correct order, the files required to deploy the admission controller in a cluster
 
 set -euo pipefail
 
 kubectl create secret generic admission-tls -n ac-workshop --type=Opaque --from-file=webhook.key --from-file=webhook.crt --dry-run=client -o yaml > ../ac-manifest/secret.yaml
-
-kubectl create secret generic bridgecrew-secret \
-   --from-literal=credentials=$bcapikey -n bridgecrew --dry-run=client -o yaml > $k8sdir/secret-apikey.yaml
 
 # Apply everything in the bridgecrew directory in the correct order
 kubectl apply -f ../ac-manifest/secret.yaml
