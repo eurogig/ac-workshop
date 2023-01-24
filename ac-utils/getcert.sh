6077ddd910a44fdfc28984f31734f980c8da3a23
eurogig <eurogig@gmail.com>
2023-01-24 15:24:01 -0500

    Improve cert call

--- ac-utils/getcert.sh
+++ ac-utils/getcert.sh
@@ -1,3 +1,3 @@
 
-openssl req -x509 -sha256 -newkey rsa:2048 -keyout webhook.key -out webhook.crt -days 1024 -nodes -addext "subjectAltName = DNS.1:validate.ac-workshop.svc"
+openssl req -x509 -sha256 -newkey rsa:2048 -keyout webhook.key -out webhook.crt -days 1024 -nodes -extensions SAN -config <(cat ac-workshop.cnf <(printf "[SAN]\nsubjectAltName=DNS.1:validate.ac-workshop.svc"))
 
